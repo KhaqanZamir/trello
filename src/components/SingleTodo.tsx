4a58b72f232642434c12b42851d3f7b61c7b5644
Reactive Space <reactivespace@Bilals-MacBook-Pro.local>
2022-06-14 17:31:53 +0500

    finished with dragging tasks

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,24 +1,26 @@
 import { faCheck, faPen, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState, useRef, useEffect } from 'react';
+import { Draggable } from 'react-beautiful-dnd';
 import './styles.css'
 import Todo from './Todo';
 
-interface Props{
+interface Props {
+    index: number;
     todo: Todo;
     todos: Todo[];
     setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-const SingleTodo: React.FC<Props> = ({todo, todos, setTodos}) => {
+const SingleTodo: React.FC<Props> = ({ index, todo, todos, setTodos }) => {
 
     const [edit, setEdit] = useState<boolean>(false);
     const [editText, setEditText] = useState<string>(todo.todo);
     const inputRef = useRef<HTMLInputElement>(null);
 
     const handleEdit = (isDone: boolean) => {
-        if( !edit && !isDone){
-            setEdit(!edit); 
+        if (!edit && !isDone) {
+            setEdit(!edit);
         }
     }
 
@@ -27,12 +29,12 @@ const SingleTodo: React.FC<Props> = ({todo, todos, setTodos}) => {
     }
 
     const handleDone = (id: number) => {
-        setTodos(todos.map((todo) => todo.id === id ? {...todo, isDone:!todo.isDone} : todo));
+        setTodos(todos.map((todo) => todo.id === id ? { ...todo, isDone: !todo.isDone } : todo));
     }
 
-    const handleEditText = (e:React.FormEvent, id:number) => {
+    const handleEditText = (e: React.FormEvent, id: number) => {
         e.preventDefault();
-        setTodos(todos.map((todo) => (todo.id === id ? {...todo, todo:editText} : todo)))
+        setTodos(todos.map((todo) => (todo.id === id ? { ...todo, todo: editText } : todo)))
         setEdit(false);
     }
 
@@ -40,23 +42,27 @@ const SingleTodo: React.FC<Props> = ({todo, todos, setTodos}) => {
         inputRef.current?.focus();
     }, [edit])
 
-  return (
-        <div className='todo'>
-            <div className='todo-header'>
-                <p>{todo.id}</p>
-                <div className='todo-icons'>
-                    <FontAwesomeIcon className='todo-icon' icon={faPen} onClick={() => handleEdit(todo.isDone)} />
-                    <FontAwesomeIcon className='todo-icon' icon={faTrashCan} onClick={() => handleDelete(todo.id)} />
-                    <FontAwesomeIcon className='todo-icon' icon={faCheck} onClick={() => handleDone(todo.id)} />
+    return (
+        <Draggable draggableId={todo.id.toString()} index={index}>
+            {(provided) => (
+                <div className='todo' {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+                    <div className='todo-header'>
+                        <p>{todo.id}</p>
+                        <div className='todo-icons'>
+                            <FontAwesomeIcon className='todo-icon' icon={faPen} onClick={() => handleEdit(todo.isDone)} />
+                            <FontAwesomeIcon className='todo-icon' icon={faTrashCan} onClick={() => handleDelete(todo.id)} />
+                            <FontAwesomeIcon className='todo-icon' icon={faCheck} onClick={() => handleDone(todo.id)} />
+                        </div>
+                    </div>
+                    <form className='edit-text-form' onSubmit={(e) => handleEditText(e, todo.id)}>
+                        {
+                            edit ? (<input ref={inputRef} value={editText} onChange={(e) => setEditText(e.target.value)} />) : todo.isDone ? <s>{todo.todo}</s> : <p>{todo.todo}</p>
+                        }
+                    </form>
                 </div>
-            </div>
-            <form className='edit-text-form' onSubmit={(e) => handleEditText(e, todo.id)}>
-                {
-                    edit ? (<input ref={inputRef} value={editText} onChange={(e) => setEditText(e.target.value)}/>) : todo.isDone ? <s>{todo.todo}</s> : <p>{todo.todo}</p>
-                }
-            </form>
-        </div>
-  )
+            )}
+        </Draggable>
+    )
 }
 
 export default SingleTodo
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,24 +1,45 @@
 import React from 'react';
+import { Droppable } from 'react-beautiful-dnd';
 import SingleTodo from './SingleTodo';
 import './styles.css'
 import Todo from './Todo';
 
-interface Props{
+interface Props {
     todos: Todo[];
+    completedTodos: Todo[];
     setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+    setCompletedTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-const TodoList: React.FC<Props> = ({todos, setTodos}) => {
+const TodoList: React.FC<Props> = ({ todos, setTodos, completedTodos, setCompletedTodos }) => {
 
-  return (
-    <div className='todos'>
-        {
-            todos.map((todo) => (
-                <SingleTodo todo={todo} todos={todos} setTodos={setTodos} />
-            ))
-        }
-    </div>
-  )
+    return (
+        <div className='all-actions'>
+            <Droppable droppableId='TodosList'>
+                {(provided) => (
+                    <div className='todos' ref={provided.innerRef} {...provided.droppableProps}>
+                        <h2>Active Tasks</h2>
+                        {todos.map((todo, index) => (
+                            <SingleTodo key={todo.id} index={index} todo={todo} todos={todos} setTodos={setTodos} />
+                        ))}
+                        {provided.placeholder}
+                    </div>
+                )}
+            </Droppable>
+
+            <Droppable droppableId='CompletedList'>
+                {(provided) => (
+                    <div className='completed-tasks' ref={provided.innerRef} {...provided.droppableProps}>
+                        <h2>Completed Tasks</h2>
+                        {completedTodos.map((todo, index) => (
+                            <SingleTodo key={todo.id} index={index} todo={todo} todos={completedTodos} setTodos={setCompletedTodos} />
+                        ))}
+                        {provided.placeholder}
+                    </div>
+                )}
+            </Droppable>
+        </div>
+    )
 }
 
 export default TodoList
